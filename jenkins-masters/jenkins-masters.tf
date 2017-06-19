61cfee348829cd80c623e7198d0c2ace6822af37
Baptiste Mathus <batmat@batmat.net>
2017-06-19 12:54:13 +0200

    Using my account

--- jenkins-masters/jenkins-masters.tf
+++ jenkins-masters/jenkins-masters.tf
@@ -1,13 +1,13 @@
 resource "digitalocean_droplet" "jenkins" {
-    
+
     count = "${var.count}"
-    
+
     image = "docker-16-04"
     name = "jenkins-${count.index + 1}"
     region = "lon1"
     size = "2gb"
-    
-    ssh_keys = [ "a4:03:fc:ac:6c:2b:28:f3:23:d3:91:5b:a5:e5:2d:2c" ]
+
+    ssh_keys = [ "56:4f:3a:5e:88:7b:26:4a:68:8f:b3:25:17:71:19:88" ]
 
     connection {
           user = "root"
